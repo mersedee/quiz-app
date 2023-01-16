487e99ccb4a1097b09232b0f52ef3d4fcd30fc0f
mersedee <mersede.rahbar@gmail.com>
2023-01-16 18:37:19 +0330

    refactor questions

--- src/components/features/Questions/index.tsx
+++ src/components/features/Questions/index.tsx
@@ -3,43 +3,68 @@ import { Steps, useSteps } from 'react-step-builder'
 import Index from '@/components/features/Questions/Question'
 import { QuestionType } from '@/types'
 import Button from '@/components/ui/Button'
+import { useGetQuestionsQuery } from '@/redux/services/QuizCategory'
+import { Loading } from '@/components/ui'
 
 interface Props {
-  questions: QuestionType[]
+  options: string
+  setOptions: any
 }
 
-const Questions = ({ questions }: Props): JSX.Element => {
+const Questions = ({ options, setOptions }: Props): JSX.Element => {
+  const { data: questions, isFetching, refetch } = useGetQuestionsQuery(options)
   const { next, current, total } = useSteps()
   const [score, setScore] = useState(0)
+  const [count, setCount] = useState(1)
+
+  console.warn(questions)
+
+  const onNext = (): void => {
+    setCount(count + 1)
+    next()
+  }
+
+  const playAgain = async (): Promise<void> => {
+    setScore(0)
+    setCount(1)
+    setOptions('')
+    await refetch()
+  }
 
   return (
-    <div className="w-full">
+    <>
+      {isFetching
+        ? <Loading />
+        : <div className="w-full">
 
-      {current === total
-        ? (
-          <div className="mt-4 text-center">
-            <div className="text-3xl">
+          {count === questions.results.length
+            ? (
+              <div className="mt-4 text-center">
+                <div className="text-3xl">
                 Your Score : <span className="font-medium">{score} / {total}</span>
-            </div>
-            <Button intent="primary" className="mx-auto mt-5">Play Again</Button>
-          </div>)
-        : (
-          <>
-            <div className="flex justify-between">
-              <div>Score: {score}</div>
-              <div>{current} of {total} Question</div>
-            </div>
-            <hr className="mt-4"/>
-            <Steps>
-              {questions.map((question: QuestionType, index: number) => (
-                <Index key={index} question={question} setScore={setScore} score={score} />
-              ))}
-            </Steps>
-            <div className="flex justify-end mt-6">
-              <Button intent="primary" onClick={next}>Next</Button>
-            </div>
-          </>)}
-    </div>
+                </div>
+                <Button intent="primary" className="mx-auto mt-5" onClick={playAgain}>Play Again</Button>
+              </div>)
+            : (
+              <>
+                <div className="flex justify-between">
+                  <div>Score: {score}</div>
+                  <div>{current} of {total} Question</div>
+                </div>
+                <hr className="mt-4"/>
+                <Steps>
+                  { questions.results.length > 0 &&
+                   questions.results.map((question: QuestionType, index: number) => (
+                     <Index key={index} question={question} setScore={setScore} score={score} />
+                   ))}
+                </Steps>
+                <div className="flex justify-end mt-6">
+                  <Button intent="primary" onClick={onNext}>Next</Button>
+                </div>
+              </>)}
+        </div>
+      }
+    </>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,17 @@
 import React, { useState } from 'react'
-import { Card, Loading } from '@/components/ui'
+import { Card } from '@/components/ui'
 import { Setting } from '@/components/features'
-import { useGetQuestionsQuery } from '@/redux/services/QuizCategory'
 import Questions from '@/components/features/Questions'
 
 const Home = (): JSX.Element => {
   const [options, setOptions] = useState<string>('')
-  const { data: questions, isFetching } = useGetQuestionsQuery(options)
-
-  console.warn('questions', questions)
-
-  const isFetched = !isFetching && questions !== null && questions.results.length > 0
 
   return (
     <div className="flex justify-center">
       <Card className="w-[50%] min-h-[400px] flex justify-center items-center">
-        {isFetching
-          ? <Loading className="mx-auto" />
-          : !isFetched ? <Setting setOptions={setOptions}/> : <Questions questions={questions.results} />
-        }
+        {options.length === 0
+          ? <Setting setOptions={setOptions}/>
+          : <Questions options={options} setOptions={setOptions} />}
       </Card>
     </div>
   )
